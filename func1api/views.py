d89f1e13af0b45b38a062ba98b87263840871227
lowenchi0509 <58016152+lowenchi0509@users.noreply.github.com>
2020-04-14 22:10:41 +0800

    Update views.py

--- func1api/views.py
+++ func1api/views.py
@@ -43,6 +43,31 @@ def callback(request):
     
                     elif mtext == '@快速選單':
                         func.sendQuickreply(event)
+              
+                    elif mtext == '@按鈕樣板':
+                        func.sendButton(event)
+    
+                    elif mtext == '@確認樣板':
+                        func.sendConfirm(event)
+    
+                    elif mtext == '@轉盤樣板':
+                        func.sendCarousel(event)
+    
+                    elif mtext == '@圖片轉盤':
+                        func.sendImgCarousel(event)
+    
+                    elif mtext == '@購買披薩':
+                        func.sendPizza(event)
+    
+                    elif mtext == '@yes':
+                        func.sendYes(event)
+            if isinstance(event, PostbackEvent):  #PostbackTemplateAction觸發此事件
+                backdata = dict(parse_qsl(event.postback.data))  #取得Postback資料
+                if backdata.get('action') == 'buy':
+                    func.sendBack_buy(event, backdata)
+                elif backdata.get('action') == 'sell':
+                    func.sendBack_sell(event, backdata)
+                     
                     else :            
                         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
     
