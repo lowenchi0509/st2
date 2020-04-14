b54a980c338b6509180227653974cdef1d8e73d1
lowenchi0509 <58016152+lowenchi0509@users.noreply.github.com>
2020-04-14 22:11:12 +0800

    Update views.py

--- func1api/views.py
+++ func1api/views.py
@@ -6,7 +6,7 @@ from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 from module import func
-
+from urllib.parse import parse_qsl
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
 
