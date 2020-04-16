2ee943ba4888566b4c3497decff159a863826a9e
lowenchi0509 <58016152+lowenchi0509@users.noreply.github.com>
2020-04-16 23:05:48 +0800

    Update func.py

--- module/func.py
+++ module/func.py
@@ -7,7 +7,7 @@ from linebot.models import ImagemapSendMessage, BaseSize, MessageImagemapAction,
 from linebot.models import TextSendMessage, AudioSendMessage, VideoSendMessage
 import datetime
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
-
+baseurl = './media/'
 def sendText(event):  #傳送文字
     try:
         message = TextSendMessage(  
