d47fb84cecf4badfa085c382ac547c012b3f4150
lowenchi0509 <58016152+lowenchi0509@users.noreply.github.com>
2020-04-16 23:19:47 +0800

    Update func.py

--- module/func.py
+++ module/func.py
@@ -7,7 +7,7 @@ from linebot.models import ImagemapSendMessage, BaseSize, MessageImagemapAction,
 from linebot.models import TextSendMessage, AudioSendMessage, VideoSendMessage
 import datetime
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
-baseurl = "https://daphne-robot-2020.herokuapp.com/media/"
+baseurl = "https://github.com/lowenchi0509/st2/tree/master/media/"
 def sendText(event):  #傳送文字
     try:
         message = TextSendMessage(  
