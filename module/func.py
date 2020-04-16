5f8b207fd3556a1e0c95b61e32410fef8ddbaaa8
lowenchi0509 <58016152+lowenchi0509@users.noreply.github.com>
2020-04-16 20:05:15 +0800

    Update func.py

--- module/func.py
+++ module/func.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from linebot import LineBotApi
 from linebot.models import TextSendMessage, ImageSendMessage, StickerSendMessage, LocationSendMessage, QuickReply, QuickReplyButton, MessageAction
 from linebot.models import TemplateSendMessage,ConfirmTemplate, MessageTemplateAction, ButtonsTemplate, PostbackTemplateAction, URITemplateAction, CarouselTemplate, CarouselColumn, ImageCarouselTemplate, ImageCarouselColumn
+from linebot.models import ImagemapSendMessage, BaseSize, MessageImagemapAction, URIImagemapAction, ImagemapArea, TemplateSendMessage, ButtonsTemplate, DatetimePickerTemplateAction
 
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 
@@ -254,5 +255,96 @@ def sendBack_sell(event, backdata):  #處理Postback
             text = '點選的是賣 ' + backdata.get('item')
         )
         line_bot_api.reply_message(event.reply_token, message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+
+
+def sendImgmap(event):  #圖片地圖
+    try:
+        image_url = 'https://i.imgur.com/Yz2yzve.jpg'  #圖片位址
+        imgwidth = 1040  #原始圖片寛度一定要1040
+        imgheight = 300
+        message = ImagemapSendMessage(
+            base_url=image_url,
+            alt_text="圖片地圖範例",
+            base_size=BaseSize(height=imgheight, width=imgwidth),  #圖片寬及高
+            actions=[
+                MessageImagemapAction(  #顯示文字訊息
+                    text='你點選了紅色區塊！',
+                    area=ImagemapArea(  #設定圖片範圍:左方1/4區域
+                        x=0, 
+                        y=0, 
+                        width=imgwidth*0.25, 
+                        height=imgheight  
+                    )
+                ),
+                URIImagemapAction(  #開啟網頁
+                    link_uri='http://www.e-happy.com.tw',
+                    area=ImagemapArea(  #右方1/4區域(藍色1)
+                        x=imgwidth*0.75, 
+                        y=0, 
+                        width=imgwidth*0.25, 
+                        height=imgheight  
+                    )
+                ),
+            ]
+        )
+        line_bot_api.reply_message(event.reply_token, message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+
+def sendDatetime(event):  #日期時間
+    try:
+        message = TemplateSendMessage(
+            alt_text='日期時間範例',
+            template=ButtonsTemplate(
+                thumbnail_image_url='https://i.imgur.com/VxVB46z.jpg',
+                title='日期時間示範',
+                text='請選擇：',
+                actions=[
+                    DatetimePickerTemplateAction(
+                        label="選取日期",
+                        data="action=sell&mode=date",  #觸發postback事件
+                        mode="date",  #選取日期
+                        initial="2019-06-01",  #顯示初始日期
+                        min="2019-01-01",  #最小日期
+                        max="2020-12-31"  #最大日期
+                    ),
+                    DatetimePickerTemplateAction(
+                        label="選取時間",
+                        data="action=sell&mode=time",
+                        mode="time",  #選取時間
+                        initial="10:00",
+                        min="00:00",
+                        max="23:59"
+                    ),
+                    DatetimePickerTemplateAction(
+                        label="選取日期時間",
+                        data="action=sell&mode=datetime",
+                        mode="datetime",  #選取日期時間
+                        initial="2019-06-01T10:00",
+                        min="2019-01-01T00:00",
+                        max="2020-12-31T23:59"
+                    )
+                ]
+            )
+        )
+        line_bot_api.reply_message(event.reply_token,message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+
+def sendData_sell(event, backdata):  #Postback,顯示日期時間
+    try:
+        if backdata.get('mode') == 'date':
+            dt = '日期為：' + event.postback.params.get('date')  #讀取日期
+        elif backdata.get('mode') == 'time':
+            dt = '時間為：' + event.postback.params.get('time')  #讀取時間
+        elif backdata.get('mode') == 'datetime':
+            dt = datetime.datetime.strptime(event.postback.params.get('datetime'), '%Y-%m-%dT%H:%M')  #讀取日期時間
+            dt = dt.strftime('{d}%Y-%m-%d, {t}%H:%M').format(d='日期為：', t='時間為：')  #轉為字串
+        message = TextSendMessage(
+            text=dt
+        )
+        line_bot_api.reply_message(event.reply_token,message)
     except:
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
